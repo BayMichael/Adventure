84a2c262ef3b36ccb84037095a01dfdecbc98a63
[Michael Bayer] <59712990+BayMichael@users.noreply.github.com>
2023-05-17 15:32:33 +0200

    Added files

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ChooseYourOwnAdventure
+{
+  class Program
+  {
+      static void Main(string[] args)
+    {
+      /* THE MYSTERIOUS NOISE */
+
+      // asking for the user's name:
+      Console.Write("What is your name?: ");
+      string name = Console.ReadLine();
+      Console.WriteLine($"Hello, {name}! Welcome to our story.");
+ 
+
+
+
+
+
+
+
+
+
+
+ 
+    }
+  }
+}
